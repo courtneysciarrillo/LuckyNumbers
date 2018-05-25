03685b09ee119f781e6c3772910866d7faf581db
courtneysciarrillo <courtney.sciarrillo@gmail.com>
2018-05-25 12:13:44 -0400

    Initial Commit

---
+++ LuckyNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LuckyNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaring variables
+            string play;
+            int min;
+            int max;
+
+            Console.WriteLine("Would you like to play Lucky Numbers?");
+            play = Console.ReadLine();
+
+            while (play == "yes")
+            {
+                Console.WriteLine("Please enter a starting number.");
+                min = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please enter an ending number.");
+                max = int.Parse(Console.ReadLine());
+
+
+
+            }
+        }
+    }
+}
